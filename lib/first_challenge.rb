845d1701657ac189707a43a02ca7fc134d1c9f7a
Menachem Aber <abermendel@gmail.com>
2018-11-27 05:06:40 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,8 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
     data.each do |attribute, value|
-      
+      if attribute == :favorite_icecream_flavors
+        
 
   #remember to return your newly altered contacts hash!
   contacts
